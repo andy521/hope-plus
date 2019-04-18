52fef771999054cf3b5ed6dbbc8f34170bf7aeb2
java-aodeng <java@aodeng.cc>
2019-04-18 14:32:13 +0800

    :wrench: code-eureka Establishment of Registry

--- cloud-eureka/src/main/java/com/hope/CloudEurekaApplication.java
+++ cloud-eureka/src/main/java/com/hope/CloudEurekaApplication.java
@@ -3,11 +3,15 @@ package com.hope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class CloudEurekaApplication {
     /** logger */
     private static final Logger LOGGER = LoggerFactory.getLogger(CloudEurekaApplication.class);
