2456ac101caecba5100086eb3bb59c77e789beef
qq18281714982 <java@aodeng.cc>
2019-04-18 23:06:24 +0800

    :page_facing_up: springcloud config cloud-config build

--- cloud-config/src/main/java/com/hope/CloudConfigApplication.java
+++ cloud-config/src/main/java/com/hope/CloudConfigApplication.java
@@ -3,6 +3,7 @@ package com.hope;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableConfigServer
 @SpringBootApplication
 public class CloudConfigApplication {
 
